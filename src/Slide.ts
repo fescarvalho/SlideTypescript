446ffa92c3d69a2128bc129cabe0cd1c4de53340
fescarvalho <fernando_scarvalho@hotmail.com>
2022-12-13 21:40:57 -0300

    finally desenvelopement

--- src/Slide.ts
+++ src/Slide.ts
@@ -84,6 +84,7 @@ export default class Slide {
     this.show(next);
   }
   pause() {
+    document.body.classList.add("paused");
     this.pausedTimeout = new Timeout(() => {
       this.timeout?.pause();
       this.paused = true;
@@ -92,6 +93,7 @@ export default class Slide {
     }, 300);
   }
   continue() {
+    document.body.classList.remove("paused");
     this.pausedTimeout?.clear();
     if (this.paused) {
       this.paused = false;
@@ -103,13 +105,14 @@ export default class Slide {
   private addControls() {
     const prevButton = document.createElement("button");
     const nextButton = document.createElement("button");
+    prevButton.innerText = "Slide Anterior";
+    nextButton.innerText = "Próximo Slide";
     this.controls.appendChild(prevButton);
     this.controls.appendChild(nextButton);
 
     this.controls.addEventListener("pointerdown", () => this.pause());
-    this.controls.addEventListener("pointerup", () => this.continue());
-    prevButton.innerText = "Slide Anterior";
-    nextButton.innerText = "Próximo Slide";
+    document.addEventListener("pointerup", () => this.continue());
+    document.addEventListener("touchend", () => this.continue());
     prevButton.addEventListener("pointerup", () => this.prev());
     nextButton.addEventListener("pointerup", () => this.next());
   }
